a1547ec757f02c1dd46e4d11edccaebfb5a8e735
lydia a <lydie.antonova@gmail.com>
2023-08-21 14:56:09 +0200

    added a cat banner!

---
+++ acr-ts-tailwind/src/components/Banner/Banner.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "./Banner.css"
+
+interface BannerProps {
+  imageUrl: string //img url. potential problem - where to host the images?
+  altText: string
+  heading: string // the text on the banner
+}
+
+const Banner: React.FC<BannerProps> = ({ imageUrl, altText, heading }) => {
+  const bannerStyles: React.CSSProperties = {
+    width: "100%",
+    height: "auto",
+  }
+
+  return (
+    <>
+      <div className="img-container">
+        <img src={imageUrl} alt={altText} className="banner-img" />
+        <div className="overlay">
+          <h2>{heading}</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Banner
--- acr-ts-tailwind/src/components/Homepage.tsx
+++ acr-ts-tailwind/src/components/Homepage.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React from "react"
+import Banner from "./Banner/Banner"
 
 const Homepage = () => {
-  return <div>Homepage</div>;
-};
+  return (
+    <>
+      <Banner
+        imageUrl="https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg"
+        altText="save the cats pls"
+        heading="Hello from cats! meow meow"
+        //here - buttons
+      />
+    </>
+  )
+}
 
-export default Homepage;
+export default Homepage
